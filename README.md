29043a2375034c1c56c9b56f17541e365644d85d
a02-trial <cipin23@gmail.com>
2025-10-26 18:08:03 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,10 @@
+# Android device tree for samsung SM-A022F (a02) A022FXXS3BVB1 (android 11) mt6739
+
+```
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+```
---
+++ twrp_a02.mk
@@ -0,0 +1,28 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+DEVICE_PATH := device/samsung/a02
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+
+# Inherit some common TWRP stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Inherit device configuration
+$(call inherit-product, $(DEVICE_PATH)/device.mk)
+
+# Charger
+PRODUCT_PACKAGES += charger_res_images
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(DEVICE_PATH)/recovery/root,recovery/root)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := twrp_a02
+PRODUCT_DEVICE := a02
+PRODUCT_MODEL := SM-A022F
+PRODUCT_BRAND := samsung
+PRODUCT_MANUFACTURER := samsung
